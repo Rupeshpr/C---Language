47231a4ff4be8813c0053090496c2c265d9897bc
Rupesh Prajapati <119660440+Rupeshpr@users.noreply.github.com>
2023-11-07 18:23:57 +0530

    Add files via upload

---
+++ Conditional.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+//if-else
+/*int main(){
+	int a=5;
+	if(a<1){
+		printf("Hello");
+	}
+	else{
+		printf("Hii");
+	}
+	return 0;
+}*/
+
+//if-else-if
+/*int main(){
+	int marks = 70;
+	if(marks>=90){
+		printf("Grade A+");
+	}
+	else if(marks>=70 && marks<90){
+		printf("Grade A");
+	}
+	else if(marks>=50 && marks<70){
+		printf("Grade B");
+	}
+	else{
+		printf("Pass");
+	}
+	return 0;
+}*/
+
+
+//Switch
+
+/*int main(){
+	int num = 3;
+	switch(num){
+		case 1:
+			printf("This is no. 1");
+			break;
+		case 2:
+			printf("This is no. 2");
+			break;
+		case 3:
+			printf("This is no. 3");
+			break;
+		default:
+			printf("Invalid Number");
+	}
+	return 0;
+}*/
