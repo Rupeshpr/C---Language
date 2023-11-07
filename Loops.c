2be3ebff8701c2fc0be21cd57d14fb776e4a44a0
Rupesh Prajapati <119660440+Rupeshpr@users.noreply.github.com>
2023-11-07 18:25:24 +0530

    Add files via upload

---
+++ Loops.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+//for
+/*int main(){
+	int i;
+	for(i=0; i<5; i++){
+		printf("Hello\n");
+	}
+	return 0;
+}
+*/
+
+//while
+//int main(){
+//	int i=0;
+//	while(i<5){
+//		printf("Hello Guys\n");
+//		i++;
+//	}
+//	return 0;
+//}
+
+//do while
+/*
+int main(){
+	int i=1;
+	do{
+		printf("Good Morning\n");
+		i++;
+	}while(i<=5);
+}*/
