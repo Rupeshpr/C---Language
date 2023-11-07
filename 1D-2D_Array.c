c130a4a81d39d4e89b5c12275ab1220bdcd5adb7
Rupesh Prajapati <119660440+Rupeshpr@users.noreply.github.com>
2023-11-07 18:23:11 +0530

    Add files via upload

---
+++ 1D-2D_Array.c
@@ -0,0 +1,87 @@
+//#include<stdio.h>
+//int main(){
+//	int i;
+//	int arr[5]={10,20,30,40,50};
+//	for(i=0; i<5; i++){
+//		printf("arr[%d]=%d\n",i,arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main(){
+//	int i;
+//	int arr[]={1,2,3,4,5,5};
+//	for(i=0; i<6; i++){
+//		printf("arr[%d]=%d\n",i,arr[i]);
+//	}
+//}
+
+//perticular array
+#include<stdio.h>
+//int main()
+//{
+//	int i;
+//	int arr[] = {10,30,70,55,99};
+//	printf("The first element is %d\n",arr[0]);
+//	return 0;
+//}
+
+
+/*int main(){
+	
+	int arr[5];
+	int i;
+	
+	printf("Enter the element of array");
+	
+	for(i=0;i<5;i++){
+		scanf("%d",&arr[i]);
+	}
+	
+	printf("Content of array\n");
+	
+	for(i=0;i<5;i++){
+		printf("%d\n",arr[i]);
+	}
+	return 0;
+}
+*/
+
+/*int main(){
+	int i,j;
+	int arr[2][3]={{10,20,30},{40,50,60}};
+	for(i=0; i<2; i++){
+		for(j=0; j<3; j++){
+			printf("arr[%d][%d]=%d\n",i,j,arr[i][j]);
+		}
+	}
+	return 0;
+}
+*/
+
+
+/*int main(){
+	int i,j;
+	int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%d\t",arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}*/
+
+/*int main(){
+	int i,j;
+	int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}*/
