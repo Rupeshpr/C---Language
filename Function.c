b026afccf2897144ab26ec8a36c4f6eb69d44a98
Rupesh Prajapati <119660440+Rupeshpr@users.noreply.github.com>
2023-11-07 18:26:36 +0530

    Add files via upload

---
+++ Function.c
@@ -0,0 +1,65 @@
+//#include<stdio.h>
+//void myFunction(string){
+//	printf("Hello");
+//	
+//}
+//int main(){
+//	myFunction();
+//}
+
+
+//#include<stdio.h>
+//int myFunction(int a,int b)
+//{
+//	return a+b;
+//}
+//int main()
+//{
+//	printf("answer is :%d",myFunction(5,7));
+//	return 0;
+//}	
+
+
+//#include<stdio.h>
+//int mul(int a, int b){
+//	return a*b;
+//}
+//int main(){
+//	int total = mul(15,5);
+//	printf("Multiplication of a and b: %d",total);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int marks(int a, int b, int c, int d, int e){
+//	int totalMarks= a+b+c+d+e;
+//	return totalMarks;
+//}
+//int main(){
+//	int ans = marks(10,20,30,40,50);
+//	printf("Total subject of Marks: %d",ans);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int subtraction(int a, int b){
+//	int sub = a-b;
+//	return sub;
+//}
+//int main(){
+//	int ans = subtraction(20,10);
+//	printf("subtraction is:%d",ans);
+//}
+
+//area of circle
+
+/*int circle(float pi=3.14 r=2){
+	area = (pi)*(r*r);
+	return area;
+}
+int main(){
+	
+}*/
+
